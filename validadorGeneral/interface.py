f664c55015d0e03ec9544440e1c6243ba9611b71
203455 <77992738+203455@users.noreply.github.com>
2022-10-15 09:14:26 -0500

    Update interface, trancisiones and add pruebas

--- validadorGeneral/interface.py
+++ validadorGeneral/interface.py
@@ -1,9 +1,58 @@
 from tkinter import *
+from tkinter import filedialog
+
+sentencias = []
+bloquesIf = []
 
 root = Tk()
+root.title('IF VALIDATOR')
+root.resizable(0,0)
+root.geometry("500x400")
 
-frame = Frame(root, width=500, height=400)
+def dividirBloques():
+    auxiliar = []
+    for sentencia in range(len(sentencias)):
+        if sentencias[sentencia].startswith('if'):
+            auxiliar.append(sentencias[sentencia])
+            parar = False
+            i=1
+            while parar == False:
+                if sentencias[sentencia].startswith('if'):
+                    parar = True
+                else:
+                    auxiliar.append(sentencias[sentencia+i])
+                    i=i+1
+            bloquesIf.append(auxiliar)
+            auxiliar.clear()
+    print(bloquesIf)
+        
 
-frame.pack()
+
+def leerArchivo(archivo):
+    encontrado=False 
+    with open(archivo, "r") as arch:
+        for linea in arch:
+            if linea.startswith('if'):
+                sentencias.append(linea)
+                encontrado=True
+            elif encontrado==True:
+                if linea.startswith('    '):
+                    sentencias.append(linea)
+                elif linea.startswith('elif') or linea.startswith('else'):
+                    sentencias.append(linea)
+                else:
+                    encontrado=False  
+    print(sentencias)             
+    dividirBloques()  
+                
+
+def subirArchivo():
+    archivo = filedialog.askopenfilename(title="abrir", filetypes=(("Archivos Python","*.py"),("Todos los archivos", "*.*")))
+    leerArchivo(archivo)
+
+indicaciones = Label(root,text="Ingresa el archivo python con sentencias if")
+indicaciones.pack()
+botonSubir = Button(root, text="Subir Archivo", command=subirArchivo)
+botonSubir.pack()
 
 root.mainloop()
---
+++ validadorGeneral/prueba.py
@@ -0,0 +1,15 @@
+dragon = "Gorila"
+
+if 3 > 2:
+    print("Ha entrado en el if correctamente")
+elif 3 == 0:
+    print("NO PUEDE ENTRAR AQUI")
+    pass
+else: 
+    pass
+
+def tratamiento():
+    pass
+
+if dragon == "Comodo":
+    pass
